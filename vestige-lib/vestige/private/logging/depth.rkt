greghendershott/vestige

commit f26d75172ad0493551a6ad53b1198858a72a9d42

Stop using chaperone-procedure
Go back to using a direct wrapper procedure.

The original motivation for switching to chaperone-procedure was
racket/class. At first that seemed like the only way (wrong). Plus I
thought maybe chaperone-procedure is the idiomatic way to install
"advice" in Racket. And finally, perhaps the racket/class special
handling of chaperone-procedure, which AFAICT was motivated by
racket/contract, would be something I'd discover elsewhere; maybe
racket/contract would have "paved the way".

However I got tired of struggling with complications like depth
tracking and tail detection, including needing a dubious work-around
for <https://github.com/racket/racket/issues/1836>.

I came up with an initial, stupid, slow way to make this work (for
some definition of "work") for racket/class. I plan to loop back later
to make it work better, as well as test with more actual racket/class
example code. Anyway, my initial thought was that racket/class was
just a nice-to-have, not a must-have.

Also: Stop passing a `depth` argument to log-args and log-results.
This was always a point of confusion, because it was only used to
format the legacy logger message (e.g. for number of > or < chars).
For everything else, depth was determined by the continuation mark
value. Now log-args and log-results look that up for the legacy
message. (This is also faster now that it's a simple
continuation-mark-set-first, as opposed to needing to look at some
sequence like '(app-mark app-mark 1 2) as was the case with the
chaperone approach.)

vestige-lib/vestige/private/logging/depth.rkt
@@ -1,13 +1,11 @@
 #lang racket/base
 
 (require (for-syntax racket/base)
-         syntax/parse/define
-         "../in-marks.rkt")
+         syntax/parse/define)
 
 (provide depth-key ;for use also by tracing/wrap.rkt
-         cms->logging-depth
-         marks->logging-depth
-         with-more-logging-depth)
+         with-more-logging-depth
+         cms->logging-depth)
 
 ;; Key used for a continuation mark to indicate the depth. Non-tail
 ;; calls of traced functions adjust the depth automatically; see
@@ -19,21 +17,16 @@
 ;; Intentionally not using make-continuation-mark-key.
 (define depth-key 'vestige-depth-continuation-mark-key)
 
-(define (cms->logging-depth cms)
-  ;; For efficiency iterate until we have a number value (don't get
-  ;; list of all marks beyond what we need).
-  (or (for/or ([v (in-marks cms depth-key)])
-        (and (number? v) v))
-      0))
-
-(define (marks->logging-depth marks)
-  (or (findf number? marks)
-      0))
-
 (define-syntax-parser with-more-logging-depth
   [(_ e:expr)
    (quasisyntax/loc this-syntax
-     (let* ([old-depth (cms->logging-depth (current-continuation-marks))]
+     (let* ([old-depth (cms->logging-depth)]
             [new-depth (add1 old-depth)])
        (with-continuation-mark depth-key new-depth
          e)))])
+
+;; Accepts #f for proc, with the same meaning as for
+;; continuation-mark-set-first: An alias for
+;; (current-continuation-marks) that can possibly work faster.
+(define (cms->logging-depth [cms #f])
+  (continuation-mark-set-first cms depth-key))

vestige-lib/vestige/example/example.rkt
@@ -175,6 +175,14 @@
                [(x) x]
                [(x y) (+ x y)])))
 
+(module kw-depth-marks racket/base
+  (require vestige/tracing/implicit)
+  (define (f #:x x) x)
+  (define (g x) (+ 1 (f #:x x)))
+  (define (h x) (g x))
+  (h 12))
+(require 'kw-depth-marks)
+
 ;; Finally stop the receiver, letting it print the accumulated output,
 ;; after module evaluations have printed results, not intermixed.
 (module stop-receiver racket/base

vestige-lib/vestige/private/tracing/class.rkt
@@ -30,26 +30,21 @@
           #:with defn-srcloc    (->srcloc-as-list this-syntax)
           #:with header-srcloc  (header-srcloc (syntax->list #'(header)))
           #:with formals-srcloc (formals-srcloc #'formals)
-          #:with positionals    (cons 'self (formals->positionals #'formals))
-          (syntax/loc this-syntax
+          #:with positionals    (formals->positionals #'formals)
+          (quasisyntax/loc this-syntax
             (begin
               (class-keyword id)
-              ;; Here racket/class need us to expand to something
-              ;; matching its method-definition grammar:
-              ;; (define-values (id) <method-proceure>). For
-              ;; <method-procedure> fortunately one choice is
-              ;; chaperone-procedure.
               (define-values (id)
-                (chaperone-procedure
-                 (lambda formals body (... ...))
-                 (make-chaperone-wrapper-proc void
+                (lambda formals
+                  ;; Stupid, slow first version; calls
+                  ;; make-wrapper-proc every time.
+                  (let ([w (make-wrapper-proc (lambda formals body (... ...))
                                               'id
                                               'defn-srcloc
                                               'header-srcloc
                                               'formals-srcloc
-                                              'positionals)
-                 chaperone-prop-key
-                 chaperone-prop-val))))])
+                                              'positionals)])
+                    (w #,@#'formals.params))))))])
        (provide definer-name)))])
 
 (define/provide-method-definer private)
@@ -63,3 +58,30 @@
 (define/provide-method-definer override-final)
 (define/provide-method-definer augment-final)
 
+(module+ example
+  (define fish%
+    (class object%
+      (init size)                ; initialization argument
+      (define current-size size) ; field
+      (super-new)                ; superclass initialization
+      (trace-define/public (get-size)
+        current-size)
+      (trace-define/public (grow amt)
+                           (println this)
+                           (set! current-size (+ amt current-size))
+                           10)
+      (trace-define/public (eat other-fish)
+        (grow (send other-fish get-size)))))
+
+  (define picky-fish%
+    (class fish% (super-new)
+      (trace-define/override (grow amt)
+                             (super grow (* 3/4 amt)))))
+
+  (define daisy (new picky-fish% [size 20]))
+  (send daisy get-size)
+  (send daisy grow 100)
+  (send daisy get-size)
+
+  (define charlie (new fish% [size 500]))
+  (send daisy eat charlie))

vestige-lib/vestige/private/tracing/forms.rkt
@@ -51,16 +51,12 @@
    ;; when necessary.)
    #:with lam            (syntax/loc this-syntax (lambda formals body ...))
    #'(let ([proc (procedure-rename lam 'name)])
-       (chaperone-procedure
-        proc
-        (make-chaperone-wrapper-proc proc
-                                     'name
-                                     'defn-srcloc
-                                     'header-srcloc
-                                     'formals-srcloc
-                                     'positionals)
-        chaperone-prop-key
-        chaperone-prop-val))])
+       (make-wrapper-proc proc
+                          'name
+                          'defn-srcloc
+                          'header-srcloc
+                          'formals-srcloc
+                          'positionals))])
 
 (define-syntax-parser trace-lambda
   [(_ formals:formals body:expr ...+)

vestige-lib/vestige/private/tracing/logging.rkt
@@ -4,6 +4,7 @@
          racket/match
          racket/string
          syntax/parse/define
+         "../logging/depth.rkt"
          "../logging/log.rkt"
          "../logging/data.rkt")
 
@@ -28,7 +29,7 @@
   (when (log?)
     (do-log-args e ...)))
 
-(define (do-log-args name tail? args kws kw-vals depth
+(define (do-log-args name tail? args kws kw-vals
                      caller called positional-syms)
   (define args-str (string-join
                     (append (match positional-syms
@@ -52,14 +53,13 @@
                                         caller called
                                         args-from args-upto)
     (with-more-logging-data #:srcloc? #f
-      (log! (~a (make-string depth #\>) " " message)))))
+      (log! (~a (make-string (cms->logging-depth) #\>) " " message)))))
 
 (define-simple-macro (log-results e:expr ...)
   (when (log?)
     (do-log-results e ...)))
 
-(define (do-log-results name results depth
-                        caller called)
+(define (do-log-results name results caller called)
   (define results-str
     (~a (match results
           [(list)   "#<void>"]
@@ -68,7 +68,7 @@
   (with-tracing-mark (make-tracing-data #f #f name results-str
                                         caller called)
     (with-more-logging-data #:srcloc? #f
-      (log! (~a (make-string depth #\<) " " results-str)))))
+      (log! (~a (make-string (cms->logging-depth) #\<) " " results-str)))))
 
 (define (make-tracing-data call? tail? name message
                            caller called

vestige-lib/vestige/private/tracing/wrap.rkt
@@ -2,55 +2,19 @@
 
 (require racket/contract
          racket/match
-         "../in-marks.rkt"
          "../logging/app.rkt"
          "../logging/depth.rkt"
          "../logging/srcloc.rkt"
          "logging.rkt")
 
-(provide make-chaperone-wrapper-proc
-         (rename-out [impersonator-prop:application-mark chaperone-prop-key])
-         chaperone-prop-val)
+(provide make-wrapper-proc)
 
-;; A value for impersonator-prop:application-mark.
-(define chaperone-prop-val (cons depth-key 'app-mark))
-
-;; Produce a wrapper proc for chaperone-procedure.
-;;
-;; The depth detection -- including tail call detection -- relies on
-;; chaperone-procedure also being called with chaperone prop/val
-;; impersonator-prop:application-mark (cons depth-key 'app-mark). That
-;; way we can detect a tail call from the marks starting with two
-;; consecutive 'app-mark values. When not a tail call, we add our own
-;; depth-key mark whose value is the depth number.
-;;
-;; Honest note to future self: The "double app-mark" pattern arose
-;; inductively/empirically -- not (yet) from a rigorous understanding
-;; of how the 'app-mark values and the number values interleave.
-;;
-;; Quote from docs, where I've capitalized PROC (the proc being
-;; wrapped) and WRAPPER-PROC (this here).
-;;
-;;   "If any prop is impersonator-prop:application-mark and if the
-;;   associated prop-val is a pair, then the call to PROC is wrapped
-;;   with with-continuation-mark using (car prop-val) as the mark key
-;;   and (cdr prop-val) as the mark value. In addition, if the
-;;   immediate continuation frame of the call to the impersonated
-;;   PROCEDURE includes a value for (car prop-val) -- that is, if
-;;   call-with-immediate-continuation-mark would produce a value for
-;;   (car prop-val) in the call’s continuation -- then the value is
-;;   also installed as an immediate value for (car prop-val) as a mark
-;;   during the call to WRAPPER-PROC (which allows tail-calls of
-;;   impersonators with respect to wrapping impersonators to be
-;;   detected within WRAPPER-PROC)."
-;;
-;; <https://docs.racket-lang.org/reference/chaperones.html#%28def._%28%28lib._racket%2Fprivate%2Fbase..rkt%29._impersonate-procedure%29%29>
-(define/contract (make-chaperone-wrapper-proc proc
-                                              name
-                                              definition-srcloc
-                                              header-srcloc
-                                              formals-srcloc
-                                              positional-syms)
+(define/contract (make-wrapper-proc proc
+                                    name
+                                    definition-srcloc
+                                    header-srcloc
+                                    formals-srcloc
+                                    positional-syms)
   (-> procedure?
       symbol?
       srcloc-as-list/c
@@ -61,46 +25,48 @@
   (define called (make-called-hash-table definition-srcloc
                                          header-srcloc
                                          formals-srcloc))
-  (define (on-args kws kw-vals args)
+  (define (apply/traced kws kw-vals args)
     (define caller (cms->caller proc))
-    ;; For efficiency, don't get full list of marks. We only care
-    ;; about those through the second one that is a number (if any).
-    ;; See match pattern below.
-    (define marks (for*/fold ([vs '()]
-                              [num? #f]
-                              #:result (reverse vs))
-                             ([v (in-marks (current-continuation-marks) depth-key)]
-                              #:final (and num? (number? v)))
-                    (values (cons v vs)
-                            (or num? (number? v)))))
-    (match marks
-      [(or
-        ;; This first pattern is, IIUC, to work around
-        ;; <https://github.com/racket/racket/issues/1836>.
-        (list* 'app-mark 'app-mark (? number?) (? number? depth) _)
-        (list* 'app-mark 'app-mark (? number? depth) _))
-       ;; Tail call: Do NOT supply a `results` proc to the chaperone,
-       ;; because we don't want to log the results and because we want
-       ;; to remain a tail call. Also, do NOT call the wrapped proc
-       ;; with a new, incremented depth-key mark.
-       (log-args name #t args kws kw-vals depth caller called positional-syms)
-       (if (null? kws)
-           (apply values         args)
-           (apply values kw-vals args))]
-      [marks
-       ;; Not a tail call: Do supply a `results` proc to the
-       ;; chaperone so we can log the results. Also, ask it to call
-       ;; the wrapped proc with an incremented depth-key mark.
-       (define old-depth (marks->logging-depth marks))
-       (define new-depth (add1 old-depth))
-       (define (on-results . results)
-         (with-continuation-mark depth-key old-depth
-           (log-results name results new-depth caller called))
-         (apply values results))
-       (log-args name #f args kws kw-vals new-depth caller called positional-syms)
-       (if (null? kws)
-           (apply values on-results 'mark depth-key new-depth         args)
-           (apply values on-results 'mark depth-key new-depth kw-vals args))]))
-  (define (plain-proc . args)          (on-args null null    args))
-  (define (kw-proc kws kw-vals . args) (on-args kws  kw-vals args))
+    (define depths (continuation-mark-set->list
+                    (current-continuation-marks)
+                    depth-key))
+    (define depth (if (null? depths) 0 (car depths)))
+    ;; Tentatively push the new depth depth:
+    (with-continuation-mark depth-key (add1 depth)
+      ;; Check for tail-call => car of depths replaced,
+      ;;  which means that the first two new marks are
+      ;;  not consecutive:
+      (let ([new-depths (continuation-mark-set->list
+                         (current-continuation-marks)
+                         depth-key)])
+        (cond
+          [(and (pair? (cdr new-depths))
+                (> (car new-depths) (add1 (cadr new-depths))))
+           ;; Tail call: reset depth and just call real-value. (This is
+           ;; in tail position to the call to `apply/traced'.) We don't
+           ;; print the results, because the original call will.
+           (log-args name #t args kws kw-vals caller called positional-syms)
+           (with-continuation-mark depth-key (car depths)
+             (if (null? kws)
+                 (apply proc args)
+                 (keyword-apply proc kws kw-vals args)))]
+          [else
+           ;; Not a tail call; push the old depth, again, to ensure that
+           ;; when we push the new depth, we have consecutive depths
+           ;; associated with the mark (i.e., set up for tail-call
+           ;; detection the next time around):
+           (log-args name #f args kws kw-vals caller called positional-syms)
+           (with-continuation-mark depth-key depth
+             (call-with-values
+              (λ ()
+                (with-continuation-mark depth-key (add1 depth)
+                  (if (null? kws)
+                      (apply proc args)
+                      (keyword-apply proc kws kw-vals args))))
+              (λ results
+                (with-continuation-mark depth-key (add1 depth)
+                  (log-results name results caller called))
+                (apply values results))))]))))
+  (define (plain-proc . args)          (apply/traced null null    args))
+  (define (kw-proc kws kw-vals . args) (apply/traced kws  kw-vals args))
   (make-keyword-procedure kw-proc plain-proc))

commit a9d6e1369243e7dd9f594b0b09373fbeb98ca195

Depth defaults to 0 when no marks
This was the case until commit f26d751 broke it. Restore it.

vestige-lib/vestige/private/logging/depth.rkt
@@ -29,4 +29,5 @@
 ;; continuation-mark-set-first: An alias for
 ;; (current-continuation-marks) that can possibly work faster.
 (define (cms->logging-depth [cms #f])
-  (continuation-mark-set-first cms depth-key))
+  (or (continuation-mark-set-first cms depth-key)
+      0))